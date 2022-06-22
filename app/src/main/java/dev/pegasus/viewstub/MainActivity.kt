0f92fade190b3aba67b67e9e7be5e663260f224b
Sohaib <sohaibahmed.hypersoft@gmail.com>
2022-06-22 18:01:00 +0500

    Initial Commit

---
+++ app/src/main/java/dev/pegasus/viewstub/MainActivity.kt
@@ -0,0 +1,48 @@
+package dev.pegasus.viewstub
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import dev.pegasus.viewstub.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        /* binding.viewStub1.inflate()
+         binding.viewStub2.inflate()
+         binding.viewStub3.inflate()
+         binding.viewStub4.inflate()*/
+
+       /* withDelay(1000) { binding.viewStub1.inflate() }
+        withDelay(1100) { binding.viewStub2.inflate() }
+        withDelay(1200) { binding.viewStub3.inflate() }
+        withDelay(1300) { binding.viewStub4.inflate() }*/
+
+
+         binding.viewStub3.setOnInflateListener { viewStub, view ->
+             binding.viewStub4.inflate()
+         }
+
+         binding.viewStub2.setOnInflateListener { viewStub, view ->
+             binding.viewStub3.inflate()
+         }
+
+         binding.viewStub1.setOnInflateListener { p0, p1 ->
+             binding.viewStub2.inflate()
+         }
+
+         binding.viewStub1.inflate()
+
+    }
+
+    private fun withDelay(delay: Long = 300, block: () -> Unit) {
+        Handler(Looper.getMainLooper()).postDelayed(block, delay)
+    }
+
+}
