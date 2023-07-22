cd44e2a79eb2fe0aec6013b7750a29165215c84b
Sohaib Ahmed <sohaibahmed.hypersoft@gmail.com>
2023-07-22 13:58:55 +0500

    Initial Commit

--- app/src/main/java/dev/pegasus/viewstub/MainActivity.kt
+++ app/src/main/java/dev/pegasus/viewstub/MainActivity.kt
@@ -19,13 +19,12 @@ class MainActivity : AppCompatActivity() {
          binding.viewStub3.inflate()
          binding.viewStub4.inflate()*/
 
-       /* withDelay(1000) { binding.viewStub1.inflate() }
-        withDelay(1100) { binding.viewStub2.inflate() }
-        withDelay(1200) { binding.viewStub3.inflate() }
-        withDelay(1300) { binding.viewStub4.inflate() }*/
+        withDelay(1000) { binding.viewStub1.inflate() }
+        withDelay(2000) { binding.viewStub2.inflate() }
+        withDelay(3000) { binding.viewStub3.inflate() }
+        withDelay(4000) { binding.viewStub4.inflate() }
 
-
-         binding.viewStub3.setOnInflateListener { viewStub, view ->
+        /* binding.viewStub3.setOnInflateListener { viewStub, view ->
              binding.viewStub4.inflate()
          }
 
@@ -37,12 +36,10 @@ class MainActivity : AppCompatActivity() {
              binding.viewStub2.inflate()
          }
 
-         binding.viewStub1.inflate()
-
+         binding.viewStub1.inflate()*/
     }
 
     private fun withDelay(delay: Long = 300, block: () -> Unit) {
         Handler(Looper.getMainLooper()).postDelayed(block, delay)
     }
-
 }
